9d6cfbdc33e1df5c9c59143e0b60f0a503d18d2d
PEYYALA VENKATESWAR REDDY <venkateswarreddypeyyala17@gmail.com>
2025-05-30 16:07:05 +0530

    Add files via upload

---
+++ src/xyz/Inputexample.java
@@ -0,0 +1,15 @@
+package xyz;
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class Inputexample {
+
+	public static void main(String[] args) {
+	LinkedList<String> students=new LinkedList<String>();
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Name of 5 students");
+	
+	
+	}
+
+}
---
+++ src/xyz/studentexample.java
@@ -0,0 +1,28 @@
+package xyz;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class studentexample {
+
+	public static void main(String[] args) {
+		HashSet<String> student= new HashSet<>();
+		Scanner sc= new Scanner(System.in);
+       while(student.size()<5) {
+        	System.out.println("enter name "+i+ ":" );
+        	String str= sc.nextLine();
+        	student.add(str);
+        	if(!student.add(name)) {
+        		System.out.println("names are already");
+        	}
+        	
+        	
+        }System.out.println("names are");
+        for(String stu:student) {
+        System.out.println(stu);
+        }
+        
+	sc.close();
+
+}
+}
